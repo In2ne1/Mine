ce9f654b233f99921482390fe64f0ec785911545
In2ne1 <50585838+In2ne1@users.noreply.github.com>
2019-08-18 16:19:28 +0800

    Update 树BTree.c

--- (树(先序遍历还原树)/树BTree.c
+++ (树(先序遍历还原树)/树BTree.c
@@ -19,3 +19,65 @@ BTNode* BinaryTreeCreate(BTDataType* src)  //int* pi //src树的节点，pi记�
 	cur->rchild = BinaryTreeCreate(src);
 	return cur;
 }
+
+void BinaryTreePrevOrder(BTNode* root)   //越下的节点越先遍历，使用递归
+{
+	if(root)
+	{ 
+	    putchar(root->data);
+	    BinaryTreePrevOrder(root->lchild);
+	    BinaryTreePrevOrder(root->rchild);
+	}
+}
+
+void BinaryTreeInOrder(BTNode* root)
+{
+	if(root)
+	{ 
+	    BinaryTreeInOrder(root->lchild);
+	    putchar(root->data);
+	    BinaryTreeInOrder(root->rchild);
+	}
+}
+
+void BinaryTreePostOrder(BTNode* root)
+{
+	if (root)
+	{
+		BinaryTreePostOrder(root->lchild);
+		BinaryTreePostOrder(root->rchild);
+		putchar(root->data);
+	}
+}
+
+void BinaryTreeDestory(BTNode* root)  //后序销毁
+{
+	if (root)
+	{
+		BinaryTreeDestory(root->lchild);
+		BinaryTreeDestory(root->rchild);
+		free(root);
+	}
+}
+
+
+int BinaryTreeSize(BTNode* root)
+{
+	if (root)
+	{
+		return BinaryTreeSize(root->lchild) + BinaryTreeSize(root->rchild) + 1;
+	}
+	return 0;
+}
+
+int BinaryTreeLeafSize(BTNode* root)
+{
+	if(root)
+	{
+		if (root->lchild == NULL && root->rchild == NULL)
+			return BinaryTreeLeafSize(root->lchild) + BinaryTreeLeafSize(root->rchild) + 1;
+		else
+			return BinaryTreeLeafSize(root->lchild) + BinaryTreeLeafSize(root->rchild);
+	}
+	return 0;
+}
