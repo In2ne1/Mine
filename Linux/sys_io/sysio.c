a989cf49a242753a2757431e2244a0e798a1d707
In2ne1 <251340469@qq.com>
2020-07-10 22:08:17 +0800

    文件IO-系统调用接口

---
+++ Linux/sys_io/sysio.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<string.h>
+#include<unistd.h>
+#include<fcntl.h>
+
+int main()
+{
+  int fd = 1;
+  umask(0);  //将当前进程的文件创建权限掩码设置为0，仅在当前进程中有效
+  //int open(const char* pathname, int flags, mode_t mode);
+  //fd = open("./test.txt", O_RDWR | O_CREAT | O_EXCL, 0777);
+  fd = open("./test.txt", O_RDWR | O_EXCL, 0777);  //防止和write冲突去掉O_CREAT
+  if(fd < 0)
+  {
+    perror("open error\n");
+    return -1;
+  }
+
+  char* ptr = "may the odds be ever in your fever\n";
+  //ssize_t write(int fd, const void* buf, size_t count);
+  int ret = write(fd, ptr, strlen(ptr));
+  if(ret < 0)
+  {
+    perror("write error\n");
+    return -1;
+  }
+  printf("ret: %d \n", ret);
+  close(fd);
+  return 0;
+}
