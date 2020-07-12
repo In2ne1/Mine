41d572c2e60bb964fb9018dd91fd3a50727a3acf
In2ne1 <251340469@qq.com>
2020-07-12 14:13:50 +0800

    dup2重定向文件描述符到标准输出

--- Linux/sys_io/dup2.c
+++ Linux/sys_io/dup2.c
@@ -5,6 +5,7 @@
 
 int main()
 {
+  /*
   umask(0);
   int fd = open("./test.txt", O_RDWR | O_CREAT, 0664);
   if(fd < 0)
@@ -16,6 +17,12 @@ int main()
   printf("fd = %d\n", fd);
   fflush(stdout);  //刷新到标准输出缓冲区
   close(fd);
+  */
+
+  FILE*fp = fopen("./test.txt", "r+");
+  fp->_fileno = 1;  //将test.txt文件流指针中的文件描述符改成标准输出的描述符
+  fwrite("la~li~hou~~~\n", 1, 12, fp);
+  fclose(fp);
   
   return 0;
 }
